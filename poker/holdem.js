3f187769f2d8cd195b9218e7beaaa7679cb758a8
swegoat <filiprolf@hotmail.com>
2018-05-07 12:35:41 +0200

    tydligen gjort ändringar och glömt pusha

--- poker/holdem.js
+++ poker/holdem.js
@@ -1,21 +1,27 @@
-const board = document.getElementById('board')
-const overHand = document.getElementById('overHand')
+const board = document.getElementById('board');
+const overHand = document.getElementById('overHand');
 let cards = ['ca', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'c11', 'c12', 'c13',
 	'ha', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'h11', 'h12', 'h13',
 	'da', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'd11', 'd12', 'd13',
-	'sa', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13',
+	'sa', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13'
 ];
 let boardArr = [];
 let state = 'flop'
 let hands = [];
 let cash  = [];
+let player = {};
 let players = prompt('How many are playing?');
 
+// Skapar spelarna och utrymme att visa korten på
 for (let i=0; i<players; i++) {
-	hands[i] = [];
-	cash[i]  = 1000;
+	player[i] = {
+		hand: [],
+		cash: 1000
+	}
+	let slot = document.createElement('h2');
+	slot.setAttribute('id', 'slot' + i);
+	overHand.appendChild(slot);
 }
-
 
 function move(source, index, target) {
 	if (index != -1) {
@@ -26,12 +32,11 @@ function move(source, index, target) {
 
 function deal() {
 	for (let i=0; i<players; i++) {
-		move(cards, (Math.random() * cards.length) | 0, hands[i]);
-		move(cards, (Math.random() * cards.length) | 0, hands[i]);
+		move(cards, (Math.random() * cards.length) | 0, player[i].hand);
+		move(cards, (Math.random() * cards.length) | 0, player[i].hand);
 	}
 }
 function progress() {
-	debugger
 	if (state === 'flop') {
 		move(cards, (Math.random() * cards.length) | 0, boardArr)
 		move(cards, (Math.random() * cards.length) | 0, boardArr)
@@ -54,7 +59,8 @@ function progress() {
 
 function reveal() {
 	for (let i=0; i<players; i++) {
-		overHand.innerHTML = hands[i]
+		let id = 'slot' + i
+		document.getElementById(id).innerHTML = player[i].hand;
 	}
 	
 }
